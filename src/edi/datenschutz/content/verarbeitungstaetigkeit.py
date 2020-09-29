2df0d9598c737010f008f7b3ec3ba7dfd4e422b7
lwalther <lwalther@novareto.de>
2020-09-29 12:02:20 +0200

    Verarbeitungstaetigkeit entfernt

--- src/edi/datenschutz/content/verarbeitungstaetigkeit.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from plone.dexterity.content import Container
-from plone.supermodel import model
-
-
-class IVerarbeitungstaetigkeit(model.Schema):
-    """ Marker interface for Verarbeitungstaetigkeit
-    """
