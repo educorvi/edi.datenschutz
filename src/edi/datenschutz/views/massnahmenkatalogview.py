534491cb2e9bf9b16e553a1425d88a59e24ce67e
seppowalther <seppo.walther@educorvi.de>
2020-10-26 14:36:20 +0100

    Working on massnahmenkatalogview

--- src/edi/datenschutz/views/massnahmenkatalogview.py
+++ src/edi/datenschutz/views/massnahmenkatalogview.py
@@ -13,5 +13,52 @@ class Massnahmenkatalogview(BrowserView):
 
     def __call__(self):
         # Implement your own actions:
+        objects = []
+
+        verfuegbarkeit_id = []
+        verfuegbarkeit_title = []
+        verfuegbarkeit_orgatechnisch = []
+        verfuegbarkeit_ansprechpartner = []
+
+        vertraulichkeit_id = []
+        vertraulichkeit_title = []
+        vertraulichkeit_orgatechnisch = []
+        vertraulichkeit_ansprechpartner = []
+
+        datenintegritaet_id = []
+        datenintegritaet_title = []
+        datenintegritaet_orgatechnisch = []
+        datenintegritaet_ansprechpartner = []
+
+        verfuegbarkeit_id = []
+        verfuegbarkeit_title = []
+        verfuegbarkeit_orgatechnisch = []
+        verfuegbarkeit_ansprechpartner = []
+
+        verfuegbarkeit_id = []
+        verfuegbarkeit_title = []
+        verfuegbarkeit_orgatechnisch = []
+        verfuegbarkeit_ansprechpartner = []
+
+        verfuegbarkeit_id = []
+        verfuegbarkeit_title = []
+        verfuegbarkeit_orgatechnisch = []
+        verfuegbarkeit_ansprechpartner = []
+
+        for i in self.context.getFolderContents():
+            iobject = i.getObject()
+            objects.append(iobject)
+        import pdb; pdb.set_trace()
+
+        for i in objects:
+            for object in i.zielerfuellung:
+                if(object == 'verfuegbarkeit'):
+                    verfuegbarkeit_id.append(i.id_massnahme)
+                    verfuegbarkeit_title.append(i.title)
+                    verfuegbarkeit_orgatechnisch.append(i.art_der_massnahme)
+                    verfuegbarkeit_ansprechpartner.append(i.ausfuehrender)
+
+        import pdb; pdb.set_trace()
+
         self.msg = _(u'A small message')
         return self.index()
