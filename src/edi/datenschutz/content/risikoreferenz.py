49430eac8caa2fc91834f3453c5938709e260aab
lwalther@novareto.de <lwalther@novareto.de>
2021-09-27 21:12:58 +0200

    "Add view: risiko-referenz-view"

--- src/edi/datenschutz/content/risikoreferenz.py
+++ src/edi/datenschutz/content/risikoreferenz.py
@@ -1,61 +1,38 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.app.multilingual.browser.interfaces import make_relation_root_path
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from plone.autoform import directives
 from plone.dexterity.content import Item
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from z3c.relationfield.schema import RelationChoice
+from zope import schema
 from zope.interface import implementer
 
-
-# from edi.datenschutz import _
-
+from edi.datenschutz import _
 
 class IRisikoreferenz(model.Schema):
     """ Marker interface and Dexterity Python Schema for Risikoreferenz
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('risikoreferenz.xml')
+    title = schema.TextLine(title=u"Titel", default=u"Risikoreferenz", required=True)
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    id = schema.TextLine(title=u"ID des Risikos", required=True)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    risikoreferenz = RelationChoice(
+        title=u"Risikoverweis",
+        description=u"Bitte wählen Sie ein Risiko auf das Sie verweisen wollen.",
+        vocabulary='plone.app.vocabularies.Catalog',
+        required=False,
+    )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
-
+    directives.widget(
+        "risikoreferenz",
+        RelatedItemsFieldWidget,
+        pattern_options={
+            "selectableTypes": ["Risiko"],
+            "basePath": make_relation_root_path,
+        },
+    )
 
 @implementer(IRisikoreferenz)
 class Risikoreferenz(Item):
