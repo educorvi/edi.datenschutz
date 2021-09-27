2383dd668d3030a00b33d273b360ed22d71496c0
lwalther@novareto.de <lwalther@novareto.de>
2021-09-27 20:34:37 +0200

    Updates

--- src/edi/datenschutz/content/datenschutzfolgenabschaetzung.py
+++ src/edi/datenschutz/content/datenschutzfolgenabschaetzung.py
@@ -13,6 +13,69 @@ def get_title(context):
     title = u'Datenschutzfolgenabschätzung: %s' % context.title
     return title
 
+@provider(IContextAwareDefaultFactory)
+def get_datenschutzbeauftragter(context):
+    return context.datenschutzbeauftragter
+
+@provider(IContextAwareDefaultFactory)
+def get_dsfa_beteiligte_person(context):
+    return context.beteiligte_personen_und_ihre_rollen
+
+@provider(IContextAwareDefaultFactory)
+def get_anmerkung_zum_status(context):
+    return context.anmerkung_zum_status
+
+@provider(IContextAwareDefaultFactory)
+def get_anlagen_beschreibung(context):
+    return context.anlagen_beschreibung
+    
+@provider(IContextAwareDefaultFactory)
+def get_datum(context):
+    return context.datumsangabe
+    
+@provider(IContextAwareDefaultFactory)
+def get_dsfa_geplante_verarbeitung(context):
+    return context.title
+
+@provider(IContextAwareDefaultFactory)
+def get_dsfa_zwecke_der_verarbeitung(context):
+    return context.zwecke
+
+@provider(IContextAwareDefaultFactory)
+def get_dsfa_rechtsgrundlagen_der_verarbeitung(context):
+    return context.rechtsgrundlagen_befugnis
+
+@provider(IContextAwareDefaultFactory)
+def get_kategorien_personenbezogener_daten(context):
+    vorlage = context.kategorien_daten
+    default = []
+    for i in vorlage:
+        i['anmerkung'] = u""
+        default.append(i)
+    return default
+
+@provider(IContextAwareDefaultFactory)
+def get_kategorien_betroffener_personen(context):
+    vorlage = context.kategorien_personen
+    default = []
+    for i in vorlage:
+        i['anmerkung'] = u""
+        default.append(i)
+    return default
+    
+@provider(IContextAwareDefaultFactory)
+def get_empfaenger_fuer_offenlegung(context):
+    vorlage = context.kategorien_empfaenger
+    default = []
+    for i in vorlage:
+        newdict = {}
+        newdict['nr'] = i['nr']
+        newdict['empfaenger'] = i['bezeichnung']
+        newdict['anlass'] = i['anmerkung']
+        newdict['anmerkung'] = u""
+        default.append(newdict)
+    return default
+
 class IDatenschutzfolgenabschaetzung(model.Schema):
     """ Marker interface and Dexterity Python Schema for Datenschutzfolgenabschaetzung
     """
--- src/edi/datenschutz/views/wordview.py
+++ src/edi/datenschutz/views/wordview.py
@@ -26,7 +26,7 @@ class Wordview(BrowserView):
         personen = self.context.beteiligte_personen_und_ihre_rollen
         if personen:
             for i in range(len(personen)):
-                key='person%s' % i+1
+                key='person%s' % str(i+1)
                 personenlist.append((key, personen[i]))
 
         transformer = ITransformer(self.context)
@@ -34,47 +34,68 @@ class Wordview(BrowserView):
         if self.context.beschreibung_massnahmen:
             transformedValue = transformer(self.context.beschreibung_massnahmen, 'text/plain')
 
+        anmerkung = self.context.anmerkung_zum_status
+        if not anmerkung:
+            anmerkung = u''
+
+        begruendung = self.context.begruendung
+        if not begruendung:
+            begruendung = u''
+
+        erlaeuterung = self.context.datenschutz_erlaeuterung
+        if not erlaeuterung:
+            erlaeuterung = u''
+
+        aktenzeichen = self.context.aktenzeichen
+        if not aktenzeichen:
+            aktenzeichen = u''
+
+        datum = self.context.datumsangabe
+        if datum:
+            datum = datum.strftime('%d.%m.%Y')
+
+        pruefdatum = self.context.pruefung_bis_wann
+        if pruefdatum:
+            pruefdatum = pruefdatum.strftime('%d.%m.%Y')
+
         context = {
             'title' : self.context.title,
             'document_id' : self.context.dokument_id,
-            'aktenzeichen': self.context.aktenzeichen,
+            'aktenzeichen': aktenzeichen,
             'verantwortlicher': self.context.verantwortlicher,
             'angaben_zu_verantwortlichen': self.context.angaben_zu_verantwortlichen,
             'status': self.context.status,
-            'anmerkung_zum_status': self.context.anmerkung_zum_status,
+            'anmerkung_zum_status': anmerkung,
             'datenschutzbeauftragter': self.context.datenschutzbeauftragter,
             'zwecke': self.context.zwecke,
             'rechtsgrundlagen_befugnis': self.context.rechtsgrundlagen_befugnis,
             'dienststelle_sachgebiet_abteilung': self.context.dienststelle_sachgebiet_abteilung,
-            'datumsangabe': self.context.datumsangabe,
+            'datumsangabe': datum,
             'datenschutz_folgenabschatzung_erforderlich': self.context.datenschutz_folgenabschatzung_erforderlich,
-            'pruefung_bis_wann': self.context.pruefung_bis_wann,
-            'begruendung': self.context.begruendung,
+            'pruefung_bis_wann': pruefdatum,
+            'begruendung': begruendung,
             'vorliegen_stellungnahme': self.context.vorliegen_stellungnahme,
-            'datenschutz_erlaeuterung': self.context.datenschutz_erlaeuterung,
+            'datenschutz_erlaeuterung': erlaeuterung,
             'beschreibung_massnahmen': transformedValue,
         }
-
-        if self.context.beschreibung_massnahmen:
-            context['beschreibung_massnahmen'] = self.context.beschreibung_massnahmen.output
 
         for i in personenlist:
             context[i[0]] = i[1]
 
         count = 0
-        for i in reversed(self.context.kategorien_personen):
+        for i in self.context.kategorien_personen:
             key = 'kategorien_personen%s' %count
             context[key] = i['bezeichnung']
             count +=1
 
         count = 0
-        for i in reversed(self.context.kategorien_daten):
+        for i in self.context.kategorien_daten:
             key = 'kategorien_daten%s' % count
             context[key] = i['bezeichnung']
             count += 1
 
         count = 0
-        for i in reversed(self.context.kategorien_empfaenger):
+        for i in self.context.kategorien_empfaenger:
             key1 = 'kategorien_empfaenger_emp%s' % count
             key2 = 'kategorien_empfaenger_offen%s' % count
             context[key1] = i['bezeichnung']
@@ -90,7 +111,7 @@ class Wordview(BrowserView):
             count += 1
 
         count = 0
-        for i in reversed(self.context.loeschfristen):
+        for i in self.context.loeschfristen:
             key = 'loeschung%s' % count
             context[key] = i['bezeichnung']
             count += 1
@@ -99,16 +120,19 @@ class Wordview(BrowserView):
         for i in reversed(self.context.anlagen_beschreibung):
             key1 = 'anlagen_bezeichnung%s' % count
             key2 = 'anlagen_anmerkung%s' % count
-            context[key1] = i['bezeichnung']
+            if not i.get('link'):
+                context[key1] = i['bezeichnung']
+            else:
+                context[key1] = "%s (%s)" % (i['bezeichnung'], i['link'])
             context[key2] = i['anmerkung']
             count += 1
 
         count = 0
-        for i in reversed(self.context.aenderungen):
+        for i in self.context.aenderungen:
             key1 = 'aenderungen_wann%s' % count
             key2 = 'aenderungen_wer%s' % count
             key3 = 'aenderungen_was%s' % count
-            context[key1] = i['wann'].strftime("%d/%m/%Y")
+            context[key1] = i['wann'].strftime("%d.%m.%Y")
             context[key2] = i['wer']
             context[key3] = i['was']
             count += 1
